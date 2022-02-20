630628b920f2d8ae149060b6834ade0fad6085fb
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2022-02-20 14:29:55 +0100

    define Instance with queryToPromise method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-const sayHello = (): void => {
-  console.log('hello');
-};
+import { createPool, PoolConfig, Pool } from 'mysql';
+
+class MRCInstance {
+  mysqlPool: Pool;
+
+  constructor(mysqlConfig: PoolConfig) {
+    this.mysqlPool = createPool(mysqlConfig);
+  }
+
+  /**
+   * Execute the given query as Promise
+   * @param query - A MySQL query.
+   * @param params - An array of parameters for the query.
+   * @returns - The result of the query.
+   */
+  queryToPromise(query: string, params: any[]): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.mysqlPool.query(query, params, (error, rows) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(rows);
+      });
+    });
+  }
+}
 
 module.exports = {
-  sayHello,
+  MRCInstance,
 };
